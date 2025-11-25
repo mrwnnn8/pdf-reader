e197de81464e6c6c2647a858204e0a67a314c355
merouane <mrwnmrwn382@gmail.com>
2025-11-25 20:37:23 +0100

    Refactor PDF reading logic and add UI button
    Refactor PDF reader code to use a function for reading pages and added a button to trigger reading.

--- PDF.py
+++ PDF.py
@@ -9,15 +9,17 @@ engine = pyttsx3.init()
 
 root = tk.Tk()
 root.title ("pdf reader")
-root.geometry = ("400x200")
+root.geometry ("400x200")
 
-Label = tk.Label(root, text = "to read either all the pages or a chosen one enter 'all' or a page number")
-p= input('enter the number of the page u wanna be read (if u want all just type "all")').strip().lower() 
-Label.pack(pady=10)
+label = tk.Label(root, text="To read all pages or a chosen one, enter 'all' or a page number")
+label.pack(pady=10)
 
 entry = tk.Entry(root)
 entry.pack(pady=5)
-if p == "all":
+
+def  readpdf(): 
+    p = entry.get().strip().lower()
+    if p == "all":
         for i in range(num):
             page= reader.pages[i]
             text = page.extract_text()
@@ -27,25 +29,28 @@ if p == "all":
 
             engine.say(text)
     
-else: 
-    try:
-        p = int(p) - 1
+    else: 
+        try:
+            p = int(p) - 1
         
-        if p<0 or p >= num :
-            print("page isn't in the range")
-        else: 
-            page= reader.pages[p]
-            text = page.extract_text()
+            if p<0 or p >= num :
+                 print("page isn't in the range")
+            else: 
+                page= reader.pages[p]
+                text = page.extract_text()
             
             print(f"==================page{p+1}==================")
             print(text)
             engine.say(text)
     
-    except ValueError :
-        print('pls enter a number or "all" ')
-    
-engine.runAndWait()
+        except ValueError :
+             print('pls enter a number or "all" ')
+    engine.runAndWait()  
 
 
+button = tk.Button(root, text="Read PDF", command=readpdf)
+button.pack(pady=20)
+
+root.mainloop()
 
 
